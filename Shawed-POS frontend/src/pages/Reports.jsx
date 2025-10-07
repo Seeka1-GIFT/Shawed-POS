57bab017cc478f03a1fc9ae78b5230cfe1e83da6
Seeka1-GIFT <cabdirxmn1100@gmail.com>
2025-10-07 05:46:19 -0700

    🚨 CRITICAL FIX: Add missing useEffect import to Reports page

--- Shawed-POS frontend/src/pages/Reports.jsx
+++ Shawed-POS frontend/src/pages/Reports.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useMemo, useState } from 'react';
+import React, { useContext, useMemo, useState, useEffect } from 'react';
 import { RealDataContext } from '../context/RealDataContext';
 import { ThemeContext } from '../context/ThemeContext';
 import ChartCard from '../components/ChartCard';
